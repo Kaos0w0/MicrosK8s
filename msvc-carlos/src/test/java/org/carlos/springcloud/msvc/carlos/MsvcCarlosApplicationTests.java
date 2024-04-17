ed39169872ef56347041713c2621c584c792c51d
Carlos Hernandez <carlos.hernandez.agudelo@correounivalle.edu.co>
2024-04-16 22:10:11 -0500

    feat(msvc_carlos): Add the microservice of carlos

---
+++ msvc-carlos/src/test/java/org/carlos/springcloud/msvc/carlos/MsvcCarlosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carlos.springcloud.msvc.carlos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCarlosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
